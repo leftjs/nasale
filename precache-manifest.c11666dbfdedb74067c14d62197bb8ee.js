fbe2c34f232cd0d4925f501d91d9004fdfadf4f9
leftjs <lefttjs@gmail.com>
2018-06-02 16:09:15 +0800

    change ignore

---
+++ precache-manifest.c11666dbfdedb74067c14d62197bb8ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "573800ef48f4f9bd3499",
+    "url": "/js/chunk-vendors.573800ef.js"
+  },
+  {
+    "revision": "0a342cbe3afdf14159da",
+    "url": "/js/app.0a342cbe.js"
+  },
+  {
+    "revision": "5ea04be5f843841ef9b2e1857678b260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12ce46f1ce273e9e2e64082c88cce757",
+    "url": "/img/logo.12ce46f1.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "573800ef48f4f9bd3499",
+    "url": "/css/chunk-vendors.a6b05d04.css"
+  },
+  {
+    "revision": "0a342cbe3afdf14159da",
+    "url": "/css/app.b5b62996.css"
+  }
+];
