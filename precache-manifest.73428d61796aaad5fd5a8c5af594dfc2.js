5a1460b9c8eef521591ad215e112adb2c96efa46
leftjs <lefttjs@gmail.com>
2018-06-02 16:18:28 +0800

    change

---
+++ precache-manifest.73428d61796aaad5fd5a8c5af594dfc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac7698433e4ff7dbdad3",
+    "url": "https://github.com/leftjs/nasale/js/chunk-vendors.ac769843.js"
+  },
+  {
+    "revision": "a12bd67b158ca5a933b0",
+    "url": "https://github.com/leftjs/nasale/js/app.a12bd67b.js"
+  },
+  {
+    "revision": "fcfc604fe0afb9feaf643f1592aa0f26",
+    "url": "https://github.com/leftjs/nasale/index.html"
+  },
+  {
+    "revision": "12ce46f1ce273e9e2e64082c88cce757",
+    "url": "https://github.com/leftjs/nasale/img/logo.12ce46f1.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "https://github.com/leftjs/nasale/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "ac7698433e4ff7dbdad3",
+    "url": "https://github.com/leftjs/nasale/css/chunk-vendors.a6b05d04.css"
+  },
+  {
+    "revision": "a12bd67b158ca5a933b0",
+    "url": "https://github.com/leftjs/nasale/css/app.9d26b9af.css"
+  }
+];
