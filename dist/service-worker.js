5b0b841d8d92782383258504bd46d958ea4e1baa
leftjs <lefttjs@gmail.com>
2018-06-02 16:18:28 +0800

    change

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c11666dbfdedb74067c14d62197bb8ee.js"
+  "https://github.com/leftjs/nasale/precache-manifest.73428d61796aaad5fd5a8c5af594dfc2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "nasale"});
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  baseUrl: "https://github.com/leftjs/nasale"
+};
